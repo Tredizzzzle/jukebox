6f4fc60ddb73aea17824fa9f104f07fd6ebd23ce
Tredizzzzle <stanleyphuynh@gmail.com>
2021-04-18 15:24:12 -0700

    Removed settings dependency

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 const bot = new Discord.Client({
   partials: ["MESSAGE", "CHANNEL", "REACTION"],
 });
-const config = require("./settings.json");
 const { loadCommands } = require("./utils/loadCommands");
 const DisTube = require("distube");
 
@@ -46,4 +45,4 @@ bot.aliases = new Discord.Collection();
 
 loadCommands(bot);
 
-bot.login(config.token);
+bot.login(token);
