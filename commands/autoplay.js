0d91dd7dab84eff319f449a788102622f92f3a61
Tredizzzzle <stanleyphuynh@gmail.com>
2021-04-18 16:05:27 -0700

    Added autoplay

---
+++ commands/autoplay.js
@@ -0,0 +1,14 @@
+module.exports.run = async (bot, message, args) => {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "You must be in a voice channel to use this command."
+    );
+
+  let mode = bot.distube.toggleAutoplay(message);
+  message.channel.send("**AUTOPLAY**" + (mode ? "**`ON`**" : "`OFF`**") + "`");
+};
+
+module.exports.config = {
+  name: "autoplay",
+  aliases: ["ap"],
+};
