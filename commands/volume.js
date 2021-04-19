f1f664ada876b8d1d51ee562113794ed3f8d3ef6
Tredizzzzle <stanleyphuynh@gmail.com>
2021-04-19 09:17:25 -0700

    Added volume command

---
+++ commands/volume.js
@@ -0,0 +1,14 @@
+module.exports.run = async (bot, message, args) => {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "You must be in a voice channel to use this command."
+    );
+  let queue = await bot.distube.getQueue(message);
+  bot.distube.setVolume(message, args[0]);
+  message.channel.send(`**VOLUME: \`${queue.volume}\`**`);
+};
+
+module.exports.config = {
+  name: "play",
+  aliases: ["p"],
+};
